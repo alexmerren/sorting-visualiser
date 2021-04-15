bef873504762a43d6649a7c92cc9843dc12c380f
Alex Merren <alexandermerren@gmail.com>
2021-04-15 04:03:28 +0100

    Added animations

--- static/js/sortingFunction.js
+++ static/js/sortingFunction.js
@@ -1,13 +1,16 @@
 /* === Quick Sort === */
 
-function quickSort(arr) {
-    let middle = arr.length / 2;
+function quickSort(arr, delay) {
     return arr;
+}
+
+function partition() {
+
 }
 
 /* === Merge Sort === */
 
-function mergeSort(arr) {
+function mergeSort(arr, delay) {
     // Find the middle of the given array.
     let middle = arr.length / 2;
     // If an array is 1 element, it is already merged completely.
@@ -43,41 +46,82 @@ function merge(leftArr, rightArr) {
 
 /* === Bubble Sort === */
 
-function bubbleSort(arr) {
-    let i, j, temp;
+async function bubbleSort(arr, delay) {
+    let i, j, temp, n;
+    n = arr.length;
     // Iterate through the entire array.
     for (i = 0; i < arr.length; ++i) {
         // Go through the remaining amount of elements
         // to the right of the current element.
         for (j = 0; j < arr.length-i-1; ++j) {
+            // Change the colour of the two values being compared to red.
+            changeColor(j, "#FF0000", delay)
+            changeColor(j+1, "#FF0000", delay)
+
+            // Wait for the previous frame to end before
+            // executing the next part of the loop.
+            await new Promise(resolve => {
+                setTimeout(() => {
+                    resolve();
+                }, delay);
+            });
+
             // If the element next in line is bigger 
             // than the current element- swap them.
             if (arr[j] > arr[j+1]) {
                 temp = arr[j];
                 arr[j] = arr[j+1];
                 arr[j+1] = temp;
+                // Animate the swapping of the values.
+                animate(j, j+1, delay);
             }
+            // Change the colour of the compared values back to normal.
+            changeColor(j, "var(--element3)", delay);
+            changeColor(j+1, "var(--element3)", delay);
         }
+        // Change the colour of the  sorted value to green.
+        changeColor(n - i - 1, "#00FF00", delay);
     }
     return arr;
 }
 
 /* === Selection Sort === */
 
-function selectionSort(arr) {
-    let i, j, minIndex, temp;
+async function selectionSort(arr, delay) {
+    let i, j, minIndex;
     // Iterate through the entire array.
     for (i = 0; i < arr.length; ++i) {
         // Set the swapping index to i.
         minIndex = i;
+        // Set the colour of the minimum value to red.
+        changeColor(i, "#FF0000", delay);
 
         // Go through the remaining subset of the array
         // to the right of the current element, and
         // find the index of the smallest element.
         for (j = i + 1; j < arr.length; ++j) {
+
+            // Set the color of the other value we are 
+            // comparing to the minimum value as red.
+            changeColor(j, "#FF0000", delay);
+
+            await new Promise(resolve => {
+                setTimeout(() => {
+                    resolve();
+                }, delay);
+            });
+
+            // Reset the colour after comparing.
+            changeColor(j, "var(--element3)", delay);
+
             if (arr[j] < arr[minIndex]) {
+                // Reset the colour of the old minimum value.
+                changeColor(minIndex, "var(--element3)", delay);
                 minIndex = j;
+                // Set the colour of the new minimum value.
+                changeColor(minIndex, "#FF0000", delay);
             }
+
         }
         // Swap the leftmost unordered element if
         // there is a smaller number further right
@@ -86,29 +130,94 @@ function selectionSort(arr) {
             temp = arr[i];
             arr[i] = arr[minIndex];
             arr[minIndex] = temp;
+            // Animate the two values swapping.
+            animate(i, minIndex, delay);
         }
+        // Change the colour of the old minimum index back to default.
+        changeColor(minIndex, "var(--element3)", delay);
+        // The value at position i is the most recently sorted,
+        // so set the colour of the value to green.
+        changeColor(i, "#00FF00", delay);
     }
     return arr;
 }
 
 /* === Insertion Sort === */
 
-function insertionSort(arr) {
+async function insertionSort(arr, delay) {
     let i, j, key;
     // Iterate through the entire array.
     for (i = 1; i < arr.length; ++i) {
         // Get the current element, and set that as the key.
         key = arr[i];
+
+        // Change the color of the currently biggest element. 
+        changeColor(i, "#FF0000", delay);
+
         j = i -1;
         // Move along the array either until you hit the beginning 
         // or you find something that is bigger than it.
         while (j >= 0 && arr[j] > key) {
+
+            // Change the colour of the element that is being compared.
+            changeColor(j, "#FF0000", delay);
+
+            await new Promise(resolve => {
+                setTimeout(() => {
+                    resolve();
+                }, delay);
+            });
+
             arr[j+1] = arr[j];
+            // Move the element down the array.
+            animate(j+1, j, delay);
+            // Change the colour of the currently compared element to normal.
+            changeColor(j, "var(--element3)", delay);
             j = j-1;
         }
         arr[j+1] = key;
     }
+
+    // After the array is sorted go through and change
+    // all of the colours to green.
+    for (i = 0; i < arr.length; ++i) {
+        await new Promise(resolve => {
+            setTimeout(() => {
+                resolve();
+            }, delay);
+        });
+        changeColor(i, "#00FF00", delay)
+    }
+
     return arr;
 }
 
-function heapSort(arr) {}
+/* === Auxiliary Functions === */
+
+function changeColor(elem, color, delay) {
+    return new Promise(resolve => {
+        let arr = $(".bar");
+        window.requestAnimationFrame(function() {
+            setTimeout(() => {
+                $(arr[elem]).css("background-color", color);
+                resolve();
+            }, delay);
+        });
+    });
+}
+
+function animate(elem1, elem2, delay) {
+    return new Promise(resolve => {
+        let arr = $(".bar");
+        let elem1Val, elem2Val;
+        window.requestAnimationFrame(function() {
+            setTimeout(() => {
+                elem1Val = arr.eq(elem1).css("height");
+                elem2Val = arr.eq(elem2).css("height");
+                $(arr[elem2]).css("height", elem1Val);
+                $(arr[elem1]).css("height", elem2Val);
+                resolve();
+            }, delay);
+        });
+    })
+}
--- static/js/sortingPage.js
+++ static/js/sortingPage.js
@@ -1,76 +1,80 @@
+
+const DEFAULT_SIZE = 30;
+const DELAY = 5;
+
 $(document).ready(function() {
     // Create an array when the page is loaded.
-    let unsortedArr = refreshArray();
+    $("#arr-size").attr("value", DEFAULT_SIZE);
+    let arr = refreshArray();
 
     // Function for pressing the refresh array button.
     $("#refresh").on('click', function() {
-        unsortedArr = refreshArray();
+        arr = refreshArray();
     })
 
     // Function for pressing enter on the refresh array button.
     $("#arr-size").on('keypress', function(e) {
-        if (e.which === 13) { unsortedArr = refreshArray() }
+        if (e.which === 13) {
+            arr = refreshArray();
+        }
+    });
+    
+    // On click function for all of the different kinds of sorts.
+    $("#quick, #merge, #bubble, #selection, #insertion").on('click', function() {
+        let id = $(this).get(0).id;
+        if (id === "quick") {
+            arr = quickSort(arr, DELAY);
+        }
+        if (id === "merge") {
+            arr = mergeSort(arr, DELAY);
+        }
+        if (id === "bubble") {
+            arr = bubbleSort(arr, DELAY);
+        }
+        if (id === "selection") {
+            arr = selectionSort(arr, DELAY);
+        }
+        if (id === "insertion") {
+            arr = insertionSort(arr, DELAY);
+        }
+        console.log(arr);
     });
 
-    // Function for pressing the bubble sort button.
-    $("#bubble").on('click', function() {
-        let sortedArr = bubbleSort(unsortedArr);
-        console.log(sortedArr);
-    });
-
-    // Function for pressing the bubble sort button.
-    $("#selection").on('click', function() {
-        let sortedArr = selectionSort(unsortedArr);
-        console.log(sortedArr);
-    });
-
-    // Function for pressing the insertion sort button.
-    $("#insertion").on('click', function() {
-        let sortedArr = insertionSort(unsortedArr);
-        console.log(sortedArr);
-    });
-
-    // Function for pressing the merge sort button.
-    $("#merge").on('click', function() {
-        let sortedArr = mergeSort(unsortedArr);
-        console.log(sortedArr);
-
-    // Function for pressing the quick sort button.
-    $("#quick").on('click', function() {
-        let sortedArr = quickSort(unsortedArr);
-        console.log(sortedArr);
-    });
-    });
 });
 
 // Function to create new array upon request, with specified attributes.
 function refreshArray() {
     // Set the default amount to 200.
-    let size = 200;
+    let size = DEFAULT_SIZE;
     let limit = 500;
 
     // If the field is empty or bigger than the limit, don't do anything.
     if (  !$("#arr-size").val() ) {}
-    else if ( $("#arr-size").val() > limit ) { $("#arr-size").val(size); }
-    else {
+    else if ( $("#arr-size").val() > limit ) {
+        $("#arr-size").val(size); 
+    } else {
         size = $("#arr-size").val();
     }
+
     // Generate a new array with given limits and size,
     // then draw that array in the html.
     let arr = generateArray(size, 100, 500);
     drawArray(arr);
+
     return arr;
 }
 
 // Function to generate an array with different specified attributes.
 function generateArray(length, min, max) {
     let arr = [];
+
     for (let i = 0; i < length; ++i) {
         // Generate a random number from the method below 
         // and add it to the array.
         let number = genRandomInt(min, max);
         arr.push(number);
     }
+
     return arr;
 }
 
@@ -80,6 +84,7 @@ function drawArray(arr) {
     let value, element;
     // Empty the canvas element so that we can add the new values.
     $target.empty();
+
     for (let i = 0; i < arr.length; ++i) {
         value = arr[i];
         // Create div's with a height of the value of the random number generated.
